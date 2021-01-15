c8ef997bd2019de753f1a1eb9ac07e2d6317c402
Ajay Pilaniya <ajay.pilaniya@paytm.com>
2021-01-15 13:19:21 +0530

    Insta feed commit

---
+++ bot/index.js
@@ -0,0 +1,48 @@
+const { Telegraf } = require('telegraf');
+let _ = require("lodash");
+let util = require("util");
+let scrapperClass = require("../lib/scrapper");
+let scrapper = new scrapperClass();
+let Q = require("q");
+
+class InstaFetch{
+    constructor(instaTools) {
+        this.instaTools = instaTools;
+        this.API_KEY = '1565409091:AAFWDvPbbfTb4SBlzlDaapSZaPWJ34wRuO4';
+        this.bot = new Telegraf(this.API_KEY);
+    }
+    launch() {
+        util.log("Launching the bot...");
+        this.bot.launch();
+    }
+    addWelcomeText() {
+        this.bot.start((ctx) => ctx.reply('Welcome to Insta Fetch. Please enter a username of instagram and we will fetch the last 10 posts of it.')); // display Welcome text when we start bot
+    }
+    addInstaCommand() {
+        let self = this;
+        this.bot.on('text', (ctx) => {
+            let input = _.get(ctx, ['update', 'message', 'text']);
+            input = this.parseInstagramUserName(input);
+            util.format('https://www.instagram.com/%s/', input);
+            Q()
+                .then(function () {
+                    return self.instaTools.getFormattedMediaURLs(input);
+                })
+                .then(function (mediaUrls) {
+                    ctx.reply('We have fetched total ' + mediaUrls.length + ' posts.');
+                    _.forEach(mediaUrls || [], function (media) {
+                        let htmlResponse = util.format('<a href="%s">%s</a>', media.display_url, media.caption);
+                        ctx.replyWithHTML(htmlResponse);
+                    })
+                })
+                .catch(function () {
+                    return ctx.reply(`Some error has occured`);
+                });
+        });
+    }
+    parseInstagramUserName(input) {
+        return input;
+    }
+}
+
+module.exports = InstaFetch;
